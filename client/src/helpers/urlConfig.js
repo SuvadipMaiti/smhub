44a3271b98e7ab0c1328ea19b86871c8a98dadbb
suvadip <suvadipmaiti5@gmail.com>
2023-04-13 02:30:35 +0530

    add client -- urlConfig.js page

---
+++ client/src/helpers/urlConfig.js
@@ -0,0 +1,12 @@
+export const apiBase = "http://localhost:8000/api/v1";
+export const host = "http://localhost:8000";
+export const apiFilepath = "http://localhost:8000";
+export const filepath = "http://localhost:3000";
+
+
+
+export const facebookClientId = "";
+export const googleClientId = "";
+
+export const RAZOR_PAY_KEY_ID = "";
+
